da8a31340377da46047f3809a3a9bcf82b0d6adb
pescocofino <134400750+pescocofino@users.noreply.github.com>
2024-08-09 10:39:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultados = document.querySelector('.caixa-resultados');
+const textoResultado = document.querySelector('.caixa-resultados');
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+    }
+]
